65a20a4b7e9e24f90f50313a06c2d5e92ece17f5
AusBruce <113293507+AusBruce@users.noreply.github.com>
2025-01-18 17:33:00 +1100

    add UsersRepository Service in DI

--- eCommerce.Infrastructure/DependencyInjection.cs
+++ eCommerce.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using eCommerce.Core.RepositoryContracts;
+using eCommerce.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
 
 
 namespace eCommerce.Infrastructure
@@ -14,6 +16,9 @@ namespace eCommerce.Infrastructure
     {
         //TO DO: Add services to the IoC container
         //Infrastructure services often include data access, caching and other low-level components.
+
+
+        services.AddSingleton<IUsersRepository, UsersRepository>();
 
         return services;
     }
