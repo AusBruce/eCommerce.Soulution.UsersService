67ae0e1926f4eb8e4a3c96cae56ed2930ddf4042
AusBruce <113293507+AusBruce@users.noreply.github.com>
2025-01-18 17:22:14 +1100

    Create Model Class and DTO class

---
+++ eCommerce.Core/DTO/RegisterRequest.cs
@@ -0,0 +1,14 @@
+﻿
+namespace eCommerce.Core.DTO
+{
+    
+  public record RegisterRequest(
+  
+      string? Email,
+  
+      string? Password,
+    
+      string? PersonName,
+  
+      GenderOptions Gender);
+}
